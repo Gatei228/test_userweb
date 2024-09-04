c99f7875593858cfc90cbc71b4011754de66c55d
Gatei228 <gette.kirill362@mail.ru>
2024-09-04 08:52:10 +0600

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3, 
+    spaceBetween: 24,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: false,
+    autoplay: false,
+});
