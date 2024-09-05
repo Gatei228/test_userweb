058098f37d7f6dcef676e92844bb320b8ae0202f
Gatei228 <gette.kirill362@mail.ru>
2024-09-05 16:20:08 +0600

    final 1.0

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,54 @@
+// swiper
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3, 
-    spaceBetween: 24,
+    slidesPerView: 1, 
+    spaceBetween: 0,
+    breakpoints: {
+        376: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        }
+    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+
     loop: false,
     autoplay: false,
+
+    preventClicksPropagation: true,
+    preventClicks: true,
+    watchOverflow: true,
+
 });
+
+//burger
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    const burgerMenu = document.querySelector('.burger-menu');
+    
+    navLinks.classList.toggle('active');
+    burgerMenu.classList.toggle('active');
+}
+
+
+// custom_select_arrow
+
+const selectElement = document.getElementById('customSelect');
+
+// открытие select
+selectElement.addEventListener('focus', function() {
+  this.classList.add('opened');
+});
+
+// закрытие select
+selectElement.addEventListener('blur', function() {
+  this.classList.remove('opened');
+});
+
+
+
+
+
+
+
